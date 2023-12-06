3616edc824fad999225b1ade94405dd149ce0a8a
Tho Kien <74567462+kin280@users.noreply.github.com>
2023-12-06 19:45:02 +0700

    Add project files.

---
+++ ungdungchuyendoitiente/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class doitien
+{
+    
+    static double Doitien(double USD, double Rate)
+    {
+        return USD * Rate;
+    }
+
+    static void Main()
+    {
+        
+        double Rate = 23000; 
+        Console.Write("Nhập giá trị tiền USD: ");
+        double USD = Convert.ToDouble(Console.ReadLine());
+
+        double VND = Doitien(USD, Rate);
+        Console.WriteLine($"{USD} USD = {VND} VNĐ");
+
+        Console.ReadLine();
+    }
+}
+
